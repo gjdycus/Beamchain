d302dd964243f1422e97c5590731a43059d9b906
Garrett Dycus <gjdycus@gmail.com>
2017-10-14 13:13:01 -0400

    Abstract away implementation

--- lib/beamchain.ex
+++ lib/beamchain.ex
@@ -1,18 +1,5 @@
 defmodule Beamchain do
-  @moduledoc """
-  Documentation for Beamchain.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Beamchain.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  defdelegate start(),              to: Beamchain.API
+  defdelegate read_blocks(pid),     to: Beamchain.API
+  defdelegate add_block(pid, data), to: Beamchain.API
 end
